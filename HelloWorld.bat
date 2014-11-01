5312ec850a4d9343c222d225ead8ebcbdee1874d
PyroAngel <eatdapoison@gmail.com>
2014-11-01 14:46:55 -0700

    I felt like making this
    Sue me! haha

---
+++ HelloWorld.bat
@@ -0,0 +1,14 @@
+@echo off
+echo Hello World!
+pause
+echo This is a quick little tutorial to show you how Batch files work
+pause
+echo ...cuz I FEEL LIKE IT!
+pause
+echo Now for a loop. Check out the code to see how I did this by right clicking the file and pressing 'edit'. 
+echo When you're done just quit CMD.
+echo Enjoy the loop!
+pause
+:loop
+echo This is the loop!
+goto loop
